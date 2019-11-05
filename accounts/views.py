1ac2ca37d6f2eafa3c9917b131b28671ed58c5f6
sd8917 <sudhanshuraj8917@gmail.com>
2019-11-05 22:53:07 +0530

    second commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 def register(request):
     
@@ -40,3 +41,27 @@ def register(request):
 
     else:
         return render(request, 'register.html')
+
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        messages.info(request,username)
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            print("user is logged in")
+            return redirect('index')
+
+        else:
+            messages.info(request,'invalid Credentials')
+            return redirect('login')
+
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
